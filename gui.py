e1addcad8940811844a41d625a6542e0362e4aa1
Vishal Gattani <vishalgattani09@gmail.com>
2023-11-11 15:07:32 -0500

    added sample sliders

--- gui.py
+++ gui.py
@@ -170,44 +170,44 @@ class App(customtkinter.CTk):
         table.grid(row=0, column=2, columnspan=1, padx=5, pady=5, sticky="")
 
         # create slider and progressbar frame
-        # self.slider_progressbar_frame = customtkinter.CTkFrame(
-        #     self, fg_color="transparent"
-        # )
-        # self.slider_progressbar_frame.grid(
-        #     row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew"
-        # )
-        # self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-        # self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-        # self.seg_button_1 = customtkinter.CTkSegmentedButton(
-        #     self.slider_progressbar_frame
-        # )
+        self.slider_progressbar_frame = customtkinter.CTkFrame(
+            self, fg_color="transparent"
+        )
+        self.slider_progressbar_frame.grid(
+            row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
+        )
+        self.slider_progressbar_frame.grid_columnconfigure(1, weight=1)
+        self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
+        self.seg_button_1 = customtkinter.CTkSegmentedButton(
+            self.slider_progressbar_frame
+        )
         # self.seg_button_1.grid(
         #     row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
         # )
-        # self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        # self.progressbar_1.grid(
-        #     row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
-        # )
-        # self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
-        # self.progressbar_2.grid(
-        #     row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
-        # )
-        # self.slider_1 = customtkinter.CTkSlider(
-        #     self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4
-        # )
-        # self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        # self.slider_2 = customtkinter.CTkSlider(
-        #     self.slider_progressbar_frame, orientation="vertical"
-        # )
-        # self.slider_2.grid(
-        #     row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns"
-        # )
-        # self.progressbar_3 = customtkinter.CTkProgressBar(
-        #     self.slider_progressbar_frame, orientation="vertical"
-        # )
-        # self.progressbar_3.grid(
-        #     row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns"
-        # )
+        self.progressbar_1 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
+        self.progressbar_1.grid(
+            row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
+        )
+        self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
+        self.progressbar_2.grid(
+            row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
+        )
+        self.slider_1 = customtkinter.CTkSlider(
+            self.slider_progressbar_frame, from_=0, to=1, number_of_steps=4
+        )
+        self.slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
+        self.slider_2 = customtkinter.CTkSlider(
+            self.slider_progressbar_frame, orientation="vertical"
+        )
+        self.slider_2.grid(
+            row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns"
+        )
+        self.progressbar_3 = customtkinter.CTkProgressBar(
+            self.slider_progressbar_frame, orientation="vertical"
+        )
+        self.progressbar_3.grid(
+            row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns"
+        )
 
         # create scrollable frame
         # self.scrollable_frame = customtkinter.CTkScrollableFrame(
@@ -236,10 +236,10 @@ class App(customtkinter.CTk):
         self.scaling_optionemenu.set("100%")
         # self.optionmenu_1.set("CTkOptionmenu")
         # self.combobox_1.set("CTkComboBox")
-        # self.slider_1.configure(command=self.progressbar_2.set)
-        # self.slider_2.configure(command=self.progressbar_3.set)
-        # self.progressbar_1.configure(mode="indeterminnate")
-        # self.progressbar_1.start()
+        self.slider_1.configure(command=self.progressbar_2.set)
+        self.slider_2.configure(command=self.progressbar_3.set)
+        self.progressbar_1.configure(mode="indeterminnate")
+        self.progressbar_1.start()
         # self.textbox.insert("0.0", "CTkTextbox\n\n" + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n" * 20)
         # self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
         # self.seg_button_1.set("Value 2")
